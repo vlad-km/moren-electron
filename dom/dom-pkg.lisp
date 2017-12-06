f3da25f08881d56d3a1ecef3057b4e773222c923
vlad-km <gm5461@gmail.com>
2017-12-06 14:11:59 +0300

    DOM package copyright notice

--- dom/dom-pkg.lisp
+++ dom/dom-pkg.lisp
@@ -1,30 +1,8 @@
 ;;; -*- mode:lisp;  coding:utf-8 -*-
 
-;;;
-;;; This file is part of the DOM package
 ;;; DOM - dom manipulation functions package
 ;;; This package is part of the MOREN Environment
-;;;
-;;; Copyright © 2017 Vladimir Mezentsev
-;;;
-;;;
-;;; MOREN environment created for rapidly programming and prototyping programs
-;;; on JSCL language (subset of Common Lisp) in yours browser.
-;;;
-;;; MOREN is free software: you can redistribute it and/or modify it under
-;;; the terms of the GNU General  Public License as published by the Free
-;;; Software Foundation,  either version  3 of the  License, or  (at your
-;;; option) any later version.
-;;;
-;;; MOREN is distributed  in the hope that it will  be useful, but WITHOUT
-;;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-;;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-;;; for more details.
-;;;
-;;; You should  have received a  copy of  the GNU General  Public License
-;;; Version 3 from  <http://www.gnu.org/licenses/>.
-;;;
-;;;
+;;; Copyright В© 2017 Vladimir Mezentsev
 ;;;
 
 
--- dom/stream-output.lisp
+++ dom/stream-output.lisp
@@ -1,30 +1,9 @@
 ;;; -*- mode:lisp; coding:utf-8  -*-
 
 
-;;; DOM output stream
-;;; This file is part of the MOREN environment
-;;; Copyright © 2017 Vladimir Mezentsev
-;;;
-;;;
-;;; MOREN environment created for rapidly programming and prototyping programs
-;;; on JSCL language (subset of Common Lisp) in yours browser.
-;;;
-;;; MOREN is free software: you can redistribute it and/or modify it under
-;;; the terms of the GNU General  Public License as published by the Free
-;;; Software Foundation,  either version  3 of the  License, or  (at your
-;;; option) any later version.
-;;;
-;;; MOREN is distributed  in the hope that it will  be useful, but WITHOUT
-;;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-;;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-;;; for more details.
-;;;
-;;; You should  have received a  copy of  the GNU General  Public License
-;;; Version 3 from  <http://www.gnu.org/licenses/>.
-;;;
-;;; JSCL   - JSCL is a Common Lisp to Javascript compiler, which is bootstrapped
-;;; from Common Lisp and executed from the browser.
-;;; https://github.com/jscl-project/jscl
+;;; DOM - dom manipulation functions package
+;;; This package is part of the MOREN Environment
+;;; Copyright В© 2017 Vladimir Mezentsev
 ;;;
 
 
