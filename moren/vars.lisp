2e87a4169007a50e3645e5da85ec539bb9351800
vlad-km <gm5461@gmail.com>
2017-12-07 08:12:44 +0300

    fix vars documentation

--- moren/vars.lisp
+++ moren/vars.lisp
@@ -1,6 +1,6 @@
 ;;; -*- mode:lisp; coding:utf-8  -*-
 
-;;;
+
 ;;; This file is part of the MOREN environment
 ;;; Moren development console
 ;;; Copyright © 2017 Vladimir Mezentsev
@@ -16,15 +16,14 @@
 
 ;;; shadow history
 (defvar *jq-history-maxlen* 100)
-(defvar *repl-hist-pathname-template* "repl-history/devrepl-")
+(defvar *repl-hist-pathname-template* "repl-history/devrepl-" "template for repl-history dump file")
+(defvar *repl-history nil "Shadow JQ-console history")
 
 ;;; keyboard
 (defvar *moren-top-menu* nil)
 (defvar *moren-top-menu-template* nil)
 (defvar *moren-popup-menu nil)
 (defvar *moren-popup-content nil)
-
-(defvar *repl-history nil "Shadow JQ-console history")
 
 ;;; output streams
 (defvar *mordev-standard-output* nil)
